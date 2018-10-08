267edeb4a179614a06a2f6545c391fc6632f4f3b
Minjun Xu <minjunxu@andrew.cmu.edu>
2018-10-08 16:02:04 -0400

    Initial files for POMDP model of Active Perception

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  7 14:04:40 2018
+
+@author: minjun
+"""
+
+if __name__ == "__main__":
+    print("done");
---
+++ pomdp.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  8 15:17:18 2018
+
+@author: minjun
+"""
+
