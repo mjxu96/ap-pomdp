8bcfabd51b65a2d3716ae15c01e3f86669579fd7
Minjun Xu <minjunxu@andrew.cmu.edu>
2018-10-09 11:57:38 -0400

    Finish all the functions and files of version 1.0

--- main.py
+++ main.py
@@ -6,5 +6,26 @@ Created on Sun Oct  7 14:04:40 2018
 @author: minjun
 """
 
+import pomdp
+
 if __name__ == "__main__":
-    print("done");
+    scan_para = [60, 360]
+    pdp = pomdp.avPOMDP(scan_para)
+    while (True):
+        input_string = input(\
+        "Input the angle and distance of a point or just type ENTER.\n")
+        if (input_string == ""):
+            input_data = None
+        else:
+            input_tmp = input_string.split()
+            if (len(input_tmp) != 2):
+                print("Invalid input.")
+                continue;
+            else:
+                input_data = [int(input_tmp[0]), float(input_tmp[1])]
+        alpha = pdp.sampleScanAngle()
+        pdp.update(input_data, alpha)
+        pdp.draw()
+        print("present scan angle is:")
+        print(alpha)
+            
--- pomdp.py
+++ pomdp.py
@@ -26,9 +26,17 @@ class avPOMDP:
         self.y_gt = np.zeros((max_range, 1))
         self.y_scan = np.ones((max_range, 1)) / max_range
         self.points = []
+        self.points_x_pos = [0]
+        self.points_y_pos = [0]
+        self.x_scan_range_1 = []
+        self.y_scan_range_1 = []
+        self.x_scan_range_2 = []
+        self.y_scan_range_2 = []
         self.now_time = -1
         self.scan_angle = []
         self.gaussian_sigma = gaussian_sigma
+        
+        self.drawInitialization()
         
     def update(self, input_data, scan_angle):
         self.now_time += 1
@@ -39,15 +47,19 @@ class avPOMDP:
         self.updatePoints()
         self.updatePredictionDistribution()
         self.updateScanAngelDistribution()
+        self.updateScanRangeDisplay()
         
     def draw(self):
-        self.drawPredictionDistribution()
         self.drawGroundTrueth()
+        self.drawPredictionDistribution()
         self.drawScanAngelDistribution()
+        self.fig.canvas.draw()
+        
+    def sampleScanAngle(self):
+        alpha = np.random.choice(self.x[:,0], p=self.y_scan[:,0])
+        return alpha
         
     def updatePoints(self):
-        for point in self.points:
-            point[2] += 1
         right_scan_bound = self.scan_angle[-1] + self.scan_range/2
         left_scan_bound = self.scan_angle[-1] - self.scan_range/2
         for point in self.points:
@@ -66,7 +78,7 @@ class avPOMDP:
                     point[3] = 1
         return
         
-    def updatePredictionDistribution(self, sigma_expand_cof=1.1):
+    def updatePredictionDistribution(self, sigma_expand_cof=1.05):
         self.y_pred = np.ones((self.max_range, 1)) / self.max_range
         for point in self.points:
             if (point[3] == 1):
@@ -77,7 +89,7 @@ class avPOMDP:
                 for x in range(mu-3*int(np.round(sigma)), 
                                mu+3*int(np.round(sigma))):
                     x = x % self.max_range
-                    coff = 1/point[1]
+                    coff = 0.1/point[1]
                     self.y_pred[x] += self.calculateGaussian(x, mu, 
                                sigma, coff)
         self.y_pred = self.y_pred / np.sum(self.y_pred)
@@ -98,8 +110,22 @@ class avPOMDP:
         self.y_scan = self.y_scan / np.sum(self.y_scan)
         return
     
+    def updateScanRangeDisplay(self):
+        left_bnd = (self.scan_angle[-1] - self.scan_range/2) % self.max_range
+        right_bnd = (self.scan_angle[-1] + self.scan_range/2) % \
+                     self.max_range
+        left_bnd = left_bnd/180 * np.pi
+        right_bnd = right_bnd/180 * np.pi
+        self.x_scan_range_1 = np.linspace(0, 10*np.cos(left_bnd), 100)
+        self.y_scan_range_1 = np.linspace(0, 10*np.sin(left_bnd), 100)
+        self.x_scan_range_2 = np.linspace(0, 10*np.cos(right_bnd), 100)
+        self.y_scan_range_2 = np.linspace(0, 10*np.sin(right_bnd), 100)
+        return
+    
     def addPoint(self, alpha, distance, points_added_time):
         self.points.append([alpha, distance, points_added_time, 0])
+        self.points_x_pos.append(distance*np.cos(alpha/180*np.pi)) 
+        self.points_y_pos.append(distance*np.sin(alpha/180*np.pi))
         points_num = np.sum(self.y_gt > 0)
         if (points_num == 0):
             self.y_gt[alpha] = 1.0
@@ -108,45 +134,66 @@ class avPOMDP:
             self.y_gt = self.y_gt * points_num / (points_num+1)
         return
     
-    def drawPredictionDistribution(self):
-        plt.figure()
-        plt.plot(self.x, self.y_pred)
-        plt.title("s-distribution")
-        plt.show()
+    def drawInitialization(self):
+        plt.ion()
+        self.fig = plt.figure()
+        self.fig_gt = self.fig.add_subplot(221)
+        self.fig_pred = self.fig.add_subplot(222)
+        self.fig_scan = self.fig.add_subplot(223)
+        self.fig_gt.set_title("Ground Truth")
+        self.fig_pred.set_title("Prob of prediction")
+        self.fig_scan.set_title("Prob of scan angle")
+        self.line_pred, = self.fig_pred.plot(self.x, self.y_pred)
+        self.line_gt, = self.fig_gt.plot(self.points_x_pos, 
+                                         self.points_y_pos,
+                                         ms=3,color='k',marker='o',ls='')
+        self.line_range_1, = self.fig_gt.plot(self.x_scan_range_1,
+                                              self.y_scan_range_1,
+                                              color='g')
+        self.line_range_2, = self.fig_gt.plot(self.x_scan_range_2,
+                                              self.y_scan_range_2,
+                                              color='g')
+        self.line_scan, = self.fig_scan.plot(self.x, self.y_scan)
         return
+    
+    def drawPredictionDistribution(self):
+        self.line_pred.set_ydata(self.y_pred)
+        self.fig_pred.set_xlim(0, self.max_range)
+        self.fig_pred.set_ylim(0, max(self.y_pred) * 1.1)
+        #self.fig_pred.title("s-distribution")
+        return
+        
     def drawGroundTrueth(self):
-        plt.figure()
-        plt.plot(self.x, self.y_gt, "ro")
-        plt.title("ground truth")
-        plt.show()
+        self.line_gt.set_data(self.points_x_pos, self.points_y_pos)
+        self.line_range_1.set_data(self.x_scan_range_1, self.y_scan_range_1)
+        self.line_range_2.set_data(self.x_scan_range_2, self.y_scan_range_2)
+        display_range = max(max(max(self.points_x_pos), 
+                                -min(self.points_x_pos)), 
+            max(max(self.points_y_pos), -min(self.points_y_pos)))
+        self.fig_gt.set_xlim(-display_range-1, display_range+1)
+        self.fig_gt.set_ylim(-display_range-1, display_range+1)
+        '''
+        self.fig_gt.set_xlim(-max(max(self.points_x_pos), 
+                                  -min(self.points_x_pos)) - 1, 
+                               max(max(self.points_x_pos), 
+                                  -min(self.points_x_pos)) + 1)
+        self.fig_gt.set_ylim(-max(max(self.points_y_pos), 
+                                  -min(self.points_y_pos)) - 1, 
+                               max(max(self.points_y_pos), 
+                                  -min(self.points_y_pos)) + 1)
+        '''
+        #self.fig_gt.title("ground truth")
+        return
     
     def drawScanAngelDistribution(self):
-        plt.figure()
-        plt.plot(self.x, self.y_scan)
-        plt.title("alpha-distribution")
-        plt.show()
+        self.line_scan.set_ydata(self.y_scan)
+        self.fig_scan.set_xlim(0, self.max_range)
+        self.fig_scan.set_ylim(0, max(self.y_scan) * 1.1)
+        #self.fig_scan.title("alpha-distribution")   
         return
     
     def calculateGaussian(self, x, mu, sigma, coff):
         y = coff * np.exp(-(x-mu)**2/(2*sigma**2)) / (np.sqrt(2*np.pi)*sigma)
         return y
-    
-# In[]
-if __name__ == "__main__":
-    scan_para = [60, 360]
-    pdp = avPOMDP(scan_para)
-    '''
-    input_data = [180, 1]
-    pdp.update(input_data, 100)
-    input_data = [100, 1]
-    pdp.update(input_data, 0)
-    input_data = [120, 1]
-    pdp.update(input_data, 100)
-    pdp.update(None, 150)
-    y_gt = pdp.y_gt
-    y_pred = pdp.y_pred
-    y_scan = pdp.y_scan
-    pdp.draw()
-    '''
-    while (True):
-        wait = input("PRESS ENTER TO CONTINUE.")
+
+            
